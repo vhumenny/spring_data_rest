282b2806a14d73d4adfe903a2925ef234d7fba4f
vhumenny <v.humenny@gmail.com>
2023-04-02 13:13:34 +0300

    spring data rest and spring boot actuator

---
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_data_rest/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.zaurtregulov.spring.springboot.spring_data_rest.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "department")
+    private String department;
+    @Column(name = "salary")
+    private int salary;
+}
